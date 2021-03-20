58544c546b7a1b2e22a53db305ec0f3e3399e8ba
Kalid Carvalho Da Silva <45861620+Kalid-Carvalho@users.noreply.github.com>
2021-03-20 19:29:50 -0400

    Criando modulo de conexão do banco de dados nosso programa

---
+++ Projeto-Sistemas-OS(Youtube)/prjinfoX/src/br/com/infox/dal/ModuloConexao.java
@@ -0,0 +1,25 @@
+package br.com.infox.dal;
+
+import java.sql.*;
+
+public class ModuloConexao {
+    // método responsavel por estabelecer a conexão com o banco.
+    public static Connection conector(){
+        java.sql.Connection conexao = null;
+        //a linha abaixo chama o driver importado em biblotecas
+        String driver = "com.mysql.jdbc.Driver";
+        // criando variaveis para armazenar informações referente ao banco
+        String url = "jdbc:mysql://localhost:3306/dbinfox";
+        String user = "root";
+        String password = "root";   
+        // estabelecendo conexão com o banco de dados
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception e) {
+            return null;
+        }
+            
+    }
+}
