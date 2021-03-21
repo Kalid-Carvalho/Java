3bafcc510a47bcdd1ff8eef9a6ebd831c55f8604
Kalid Carvalho Da Silva <45861620+Kalid-Carvalho@users.noreply.github.com>
2021-03-20 20:24:09 -0400

    Criando modulo de conexão no app sorveteria

--- Projeto-Sorvetes(Pessoal)/AppSorvete/src/br/com/sorveteria/dao/ModuloConexao.java
+++ Projeto-Sorvetes(Pessoal)/AppSorvete/src/br/com/sorveteria/dao/ModuloConexao.java
@@ -1,26 +1,29 @@
-package br.com.infox.dal;
+package br.com.sorveteria.dao;
 
 import java.sql.*;
 
 public class ModuloConexao {
     // método responsavel por estabelecer a conexão com o banco.
     public static Connection conector(){
+        //cria variavel conection
         java.sql.Connection conexao = null;
         //a linha abaixo chama o driver importado em biblotecas
         String driver = "com.mysql.jdbc.Driver";
         // criando variaveis para armazenar informações referente ao banco
-        String url = "jdbc:mysql://localhost:3306/dbinfox";
+        String url = "jdbc:mysql://localhost:3306/sorveteria";
+        //criando variavel que armazena login do banco de dados
         String user = "root";
-        String password = "root";   
-        // estabelecendo conexão com o banco de dados
+        //Variavel que armazena senha do banco de dados
+        String password = "root";        
+        //Estabelecendo conexão com o banco de dados
         try {
             Class.forName(driver);
             conexao = DriverManager.getConnection(url, user, password);
             return conexao;
         } catch (Exception e) {
-           // System.out.println(e);
+            //Instrução de apoio para mostrar erro que está dando no banco de dados
+            //System.out.println(e);
             return null;
         }
-            
     }
 }
